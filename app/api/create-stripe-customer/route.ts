f2afe1736188dbc12ca329b75c4cd0a734801558
regreg0706 <regreg0706@gmail.com>
2024-11-10 22:50:31 +0900

    done4

--- app/api/create-stripe-customer/route.ts
+++ app/api/create-stripe-customer/route.ts
@@ -1,10 +1,9 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { supabaseRouteHandlerClient } from "@/utils/supabaseRouteHandlerClient";
 import { NextRequest, NextResponse } from "next/server";
 import initiStripe from "stripe";
-import { cookies } from "next/headers";
 
 export async function POST(req: NextRequest) {
-	const supabase = createRouteHandlerClient({ cookies });
+	const supabase = supabaseRouteHandlerClient();
 	const query = req.nextUrl.searchParams.get("API_ROUTE_SECRET");
 	if (query !== process.env.API_ROUTE_SECRET) {
 		return NextResponse.json({
@@ -13,10 +12,10 @@ export async function POST(req: NextRequest) {
 	}
 
 	const data = await req.json();
-  const { id,email } = data.record;
+	const { id,email } = data.record;
 
-  const stripe = new initiStripe(process.env.STRIPE_SECRET_KEY!);
-  const customer = await stripe.customers.create({ 
+	const stripe = new initiStripe(process.env.STRIPE_SECRET_KEY!);
+	const customer = await stripe.customers.create({ 
 		email,
 	 });
 
