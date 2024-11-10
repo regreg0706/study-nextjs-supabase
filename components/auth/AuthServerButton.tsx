2502adea2a5c1d1ad6b3afe1387e59b45f227f76
regreg0706 <regreg0706@gmail.com>
2024-11-11 00:34:42 +0900

    done16

--- components/auth/AuthServerButton.tsx
+++ components/auth/AuthServerButton.tsx
@@ -1,13 +1,13 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
+import { supabaseServer } from "@/utils/supabaseServer";
 import AuthClientButton from "./AuthClientButton";
 
 const AuthServerButton = async () => {
-  const supabase = createServerComponentClient( { cookies } );
-  const { data: { user } } = await supabase.auth.getUser();
+  const supabase = supabaseServer();
+  const { data: { user } } = await supabase.auth.getSession();
+  const session = user.session;
 
   return (
-    <AuthClientButton session={user} />
+    <AuthClientButton session={session} />
   );
 }
 
