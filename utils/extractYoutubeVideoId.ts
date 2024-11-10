fb8daba52e14fba030f8a9ab0c82ba2e8b324084
regreg0706 <regreg0706@gmail.com>
2024-11-11 00:39:33 +0900

    done18

--- utils/extractYoutubeVideoId.ts
+++ utils/extractYoutubeVideoId.ts
@@ -1,14 +1,12 @@
 export function extractYouTubeVideoId(url: string): string | null {
-    const matched =
-      /^https?:\/\/(www\.)?youtube\.com\/watch\?(.*&)?v=(?<videoId>[^&]+)/.exec(
-        url
-      ) ??
-      /^https?:\/\/youtu\.be\/(?<videoId>[^?]+)/.exec(url) ??
-      /^https?:\/\/(www\.)?youtube\.com\/embed\/(?<videoId>[^?]+)/.exec(url);
-  
-    if (matched?.groups?.videoId) {
-      return matched.groups.videoId;
-    } else {
-      return null;
-    }
+  const match =
+    /^https?:\/\/(www\.)?youtube\.com\/watch\?(.*&)?v=([^&]+)/.exec(url) ||
+    /^https?:\/\/youtu\.be\/([^?]+)/.exec(url) ||
+    /^https?:\/\/(www\.)?youtube\.com\/embed\/([^?]+)/.exec(url);
+
+  if (match) {
+    return match[3] || match[1] || match[2];
+  } else {
+    return null;
   }
+}
